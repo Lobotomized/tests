12d88b9a7e158615c3bd9f8a1abe2aceec5f8728
Lobotomized <ivan@writecraft.io>
2026-01-19 19:21:58 +0200

    sources cleanup

--- src/lib/server/quiz/sources.ts
+++ src/lib/server/quiz/sources.ts
@@ -280,7 +280,7 @@ export const textSources: TextSources = {
 	thisInArrowFunctions: {
 		text: `
 			Arrow functions do not have their own "this" keyword.<br/>
-			Instead, they inherit the "this" from one level above them and remember it after that, no matter the way in which they were called. 
+			Instead, they inherit the "this" from one level above them and remember it after that. 
 		`,
 		title: '"this" binding in arrow functions'
 	},
